1ca84d223c6246bd50281a3d4448fec422459f13
zuguorui <993624691@qq.com>
2019-07-31 18:35:10 +0800

    complete engine and player creation

--- app/src/main/cpp/SLAudioPlayer.cpp
+++ app/src/main/cpp/SLAudioPlayer.cpp
@@ -4,19 +4,119 @@
 
 #include "SLAudioPlayer.h"
 
+void bqPlayerCallback(SLAndroidSimpleBufferQueueItf bq, void *context)
+{
+
+}
+
 bool SLAudioPlayer::createEngine() {
-    SLresult result = 0;
+    bool finalResult = true;
+    SLresult result;
     result = slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
     result = (*engineObject)->Realize(engineObject, SL_BOOLEAN_FALSE);
-    result = (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &engineItf);
-    return result == SL_RESULT_SUCCESS;
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &engineEngine);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    SLInterfaceID ids[1] = {SL_IID_OUTPUTMIX};
+    SLboolean reqs[1] = {SL_BOOLEAN_FALSE};
+    result = (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 0, ids, reqs);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    return finalResult;
 }
 
 bool SLAudioPlayer::createPlayer() {
-    if(engineItf == NULL)
-    {
-        return false;
-    }
+    bool finalResult = true;
     SLresult result;
+
+    SLDataLocator_AndroidSimpleBufferQueue bufferQueue = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
+    SLDataFormat_PCM pcmFormat = {SL_DATAFORMAT_PCM, 2, SL_SAMPLINGRATE_44_1, SL_PCMSAMPLEFORMAT_FIXED_16, SL_PCMSAMPLEFORMAT_FIXED_16 * 2,
+                               SL_SPEAKER_FRONT_CENTER, SL_BYTEORDER_LITTLEENDIAN};
+
+    SLDataSource audioSrc = {&bufferQueue, &pcmFormat};
+
+    SLDataLocator_OutputMix locOutputMix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
+    SLDataSink audioSink = {&locOutputMix, NULL};
+
+    SLInterfaceID ids[2] = {SL_IID_BUFFERQUEUE, SL_IID_VOLUME};
+    SLboolean reqs[2] = {SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE};
+
+    result = (*engineEngine)->CreateAudioPlayer(engineEngine, &bqPlayerObject, &audioSrc, &audioSink, 2, ids, reqs);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*bqPlayerObject)->Realize(bqPlayerObject, SL_BOOLEAN_FALSE);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_PLAY, &bqPlayerPlay);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_BUFFERQUEUE, &bqPlayerBufferQueue);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*bqPlayerBufferQueue)->RegisterCallback(bqPlayerBufferQueue, bqPlayerCallback, NULL);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_VOLUME, &bqPlayerVolume);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    result = (*bqPlayerPlay)->SetPlayState(bqPlayerPlay, SL_PLAYSTATE_PLAYING);
+    if(result != SL_RESULT_SUCCESS)
+    {
+        finalResult = false;
+    }
+    (void*)result;
+
+    return finalResult;
 
 }
--- app/src/main/cpp/SLAudioPlayer.h
+++ app/src/main/cpp/SLAudioPlayer.h
@@ -5,6 +5,8 @@
 #ifndef OPENSLTEST_SLAUDIOPLAYER_H
 #define OPENSLTEST_SLAUDIOPLAYER_H
 
+#include <iostream>
+#include <stdlib.h>
 #include <SLES/OpenSLES.h>
 #include <SLES/OpenSLES_Android.h>
 
@@ -15,10 +17,22 @@ public:
     bool createPlayer();
 
 private:
-    SLPlayItf playerItf = NULL;
-    SLEngineItf engineItf = NULL;
+    // engine interfaces
     SLObjectItf engineObject = NULL;
-    SLObjectItf playerObject = NULL;
+    SLEngineItf engineEngine;
+
+// output mix interfaces
+    SLObjectItf outputMixObject = NULL;
+    SLEnvironmentalReverbItf outputMixEnvironmentalReverb = NULL;
+
+    SLObjectItf bqPlayerObject = NULL;
+    SLPlayItf bqPlayerPlay;
+    SLAndroidSimpleBufferQueueItf bqPlayerBufferQueue;
+    SLVolumeItf bqPlayerVolume;
+    SLmilliHertz bqPlayerSampleRate = 0;
+
+    static const SLEnvironmentalReverbSettings reverbSettings =
+            SL_I3DL2_ENVIRONMENT_PRESET_STONECORRIDOR;
 };
 
 
